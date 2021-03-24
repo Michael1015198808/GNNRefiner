51e4863eda4b12d44ac5c14de3fa25998727a472
Zhenyu Yan <1015198808@qq.com>
2021-03-24 14:00:16 +0800

    fea(processor): automatically mark unseen nodes and edges' types.

--- processor.py
+++ processor.py
@@ -4,27 +4,34 @@ import torch
 from args import NODES_TYPE_DICT, NODES_TYPE_CNT, EDGES_TYPE_DICT, EDGES_TYPE_CNT
 
 class GraphPreprocessor(object):
-    def __init__(self, cons_name: str, goal_name: str, in_name: str, device):
+    def __init__(self, cons_name: str, goal_name: str, in_name: str, device, graph_name = None):
+        self.graph_name = graph_name
         self.nodes_cnt = 0
         self.nodes: List[str] = []
-        edges_type: List[int] = []
         nodes_type: List[int] = []
+        cons = []
         self.nodes_dict:Dict[str, int] = {}
 
-        edges: List[Tuple[int, int]] = []
+        self.process_in(in_name)
+        self.process_goal(goal_name)
+        self.process_cons(cons_name, cons, nodes_type)
+        self.process_other(nodes_type, cons, device)
 
+    def process_in(self, in_name):
         self.invoke_sites = []
         self.in_set = set()
         with open(in_name, 'r') as f:
             for line in f.read().splitlines():
                 self.in_set.add(line)
 
+    def process_goal(self, goal_name):
         self.goal_set = set()
         with open(goal_name, 'r') as f:
             for line in f.read().splitlines():
                 self.goal_set.add(line)
 
-        cons = []
+    def process_cons(self, cons_name, cons, nodes_type):
+        miss = {}
         with open(cons_name, 'r') as f:
             for line in f:
                 line = line.strip()
@@ -35,7 +42,21 @@ class GraphPreprocessor(object):
                         self.nodes_dict[term] = self.nodes_cnt
                         self.nodes.append(term)
                         self.nodes_cnt += 1
-                        nodes_type.append(NODES_TYPE_DICT[term.split("(")[0]])
+                        try:
+                            nodes_type.append(NODES_TYPE_DICT[term.split("(")[0]])
+                        except KeyError as e:
+                            k = term.split("(")[0]
+                            v = len(NODES_TYPE_DICT)
+                            miss[k] = v
+                            NODES_TYPE_DICT[k] = v
+        if len(miss):
+            print(miss)
+            exit(-1)
+
+    def process_other(self, nodes_type, cons, device):
+        miss = {}
+        edges_type: List[int] = []
+        edges: List[Tuple[int, int]] = []
 
         self.nodes_fea = torch.zeros((self.nodes_cnt, NODES_TYPE_CNT + 2), dtype=torch.float32, device=device)
 
@@ -54,15 +75,29 @@ class GraphPreprocessor(object):
             head_idx = self.nodes_dict[head]
             head_type = head.split("(")[0]
             for tail in tails:
-                tail_idx = self.nodes_dict[tail]
-                tail_type = tail.split("(")[0]
-                edges.append((tail_idx, head_idx))
-                edges_type.append(EDGES_TYPE_DICT[tail_type + ">" + head_type])
-                edges.append((head_idx, tail_idx))
-                edges_type.append(EDGES_TYPE_DICT[tail_type + "<" + head_type])
+                try:
+                    tail_idx = self.nodes_dict[tail]
+                    tail_type = tail.split("(")[0]
+                    edges.append((tail_idx, head_idx))
+                    edges_type.append(EDGES_TYPE_DICT[tail_type + ">" + head_type])
+                    edges.append((head_idx, tail_idx))
+                    edges_type.append(EDGES_TYPE_DICT[tail_type + "<" + head_type])
+                except KeyError as e:
+                    k1 = tail_type + ">" + head_type
+                    k2 = tail_type + "<" + head_type
+                    v = len(NODES_TYPE_DICT)
+                    miss[k1] = v
+                    NODES_TYPE_DICT[k1] = v
+                    miss[k2] = v + 1
+                    NODES_TYPE_DICT[k2] = v + 1
+        if len(miss):
+            print(miss)
+            exit(-1)
 
         self.edges = torch.tensor(edges, dtype=torch.int64, device=device).T
         self.nodes_type = torch.tensor(nodes_type, device=device)
         self.edges_type = torch.tensor(edges_type, device=device)
+        if self.graph_name:
+            print(self.graph_name)
         print(self.nodes_cnt, "nodes.")
         print(len(edges), "edges.")
