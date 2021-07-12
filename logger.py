a4634047cef62f95c0400d090ee15ec05a0327b4
Zhenyu Yan <1015198808@qq.com>
2021-07-12 21:07:43 +0800

    (fea): Add a Timer class.

--- logger.py
+++ logger.py
@@ -1,4 +1,11 @@
-from time import ctime
+from time import ctime, time
 
 def log(*args, **kwargs):
     print(ctime(), *args, **kwargs)
+
+class Timer:
+    def __init__(self):
+        self.t = time()
+    def log(self, *args, **kwargs):
+        print(time() - self.t, *args, **kwargs)
+        self.t = time()
